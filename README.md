2a12c212a4b6c498ab54da768f1ad82429db2c6e
ShadowNight666 <shadownight666al@gmail.com>
2019-01-23 22:11:05 +0100

    Add main code

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# BeezCode
-Easy to use syntax highlighting
+
+![](https://github.com/ShadowNight666/BeezCode/images/banner.png)
+# Overview
+
+BeezCode is a simple tool that allows you to put syntax highlighting to your codes on your sites. This is internally configurable if you want to have more syntax or change something.
+
+# Installation
+
+Import BeezCode into your html such as:
+```html
+<script  src="./BeezCode.js"></script>
+```
+
+To use BeezCode, add your code in a ``<pre>`` tag with the class ``beez``, like:
+```html
+<pre class="beez">
+class regex:
+	def __init__(self, obj, regex):
+	  self.obj = obj
+	  self.regex = regex
+  
+	def __call__(self, *args):
+	  def regex_method(text=None):
+		return [x.strip() for x in self.regex.findall(text or self.obj.text)]
+	  return regex_method
+</pre>
+```
+
+# Configuration
+
+BeezCode has 2 parts: the configuration part and the syntax part.
+To create a new syntax, simply add in the variable ``syntaxes``:
+
+```json
+your_group_name: {
+
+	// Liste of your patterns as regex
+	patterns: [
+		"my",
+		"patterns",
+		"as",
+		"regex",
+		"here"
+	],
+	
+	// All css. You can add what you want as style. Think to replace "-" in your style name with "_"
+	css: {
+		color:  "#246fe2"
+	}
+}
+```
+You can create a "dynamic" pattern, like:
+```json
+{"your pattern": {
+	get: [1]
+}}
+```
+The "get" method will return matched group (in the example above, it will return the matched group ``1`` as pattern). You can return a lot of group because it's an array.
--- src/BeezCode.js
+++ src/BeezCode.js
@@ -1,23 +1,106 @@
-const background = "gray"
+const config = {
+	css: {
+		background_color: "#161719",
+		font_size: "15px",
+		color: "white"
+	}
+	
+}
+
 const syntaxes = {
-    "^\\b(class|ram)\\b": "red",
-    "regex": "blue",
-    "return": "green"
+	declarations: {
+		patterns: [
+			"const",
+			"var",
+			"let",
+			"public",
+			"private",
+			"protected",
+			"this",
+			"self"
+		],
+		css: {
+			color: "#246fe2"
+		}
+	},
+	functions: {
+		patterns: [
+			"function",
+			"defp?"
+		],
+		css: {
+			color: "#d10ac7"
+		}
+	},
+	imports: {
+		patterns: [
+			"require",
+			"import",
+			
+			{
+				"class (\\w+)": {
+					get: [1]
+				}
+			}
+		],
+		css: {
+			color: "#fbff23",
+			font_weight: "bold"
+		}
+	},
+	returns: {
+		patterns: [
+			"return"
+		],
+		css: {
+			color: "#6497e5"
+		}
+	}
 }
 
 window.onload = function() {
-    let codes = document.getElementsByClassName("beez")
-    for (i = 0; i < codes.length; i++) {
-        codes[i].style["background-color"] = background
-        let code = codes[i].innerHTML
+	let codes = document.getElementsByClassName("beez")
+	for (i = 0; i < codes.length; i++) {
+		for (setting in config.css) {
+			codes[i].style[setting.replace(/\_/gm, "-")] = config.css[setting]
+		}
 
-        for (syntax in syntaxes) {
-            let match = new RegExp(syntax, "gm")
-            
-            code = code.replace(match, '<span style="color: ' + syntaxes[syntax] + '">$&</span>')
-            
-        }
+		let code = codes[i].innerHTML
 
-        codes[i].innerHTML = code
-    }
-};
+		for (group in syntaxes) {
+
+			syntaxes[group].patterns.forEach(pattern => {
+				for (attribut in syntaxes[group].css) {
+					if (typeof pattern === "object") {
+						for (pattern in syntaxes[group].patterns.find(o => typeof o === "object")) {
+							syntaxes[group].patterns.find(o => typeof o === "object")[pattern].get.forEach(match => {
+								if (code.match(pattern)) {
+									let matcher = code.match(pattern)[0]
+									matcher = matcher.replace(
+										code.match(pattern)[match],
+										'<span style="' + attribut.replace(/\_/gm, "-") + ': ' + syntaxes[group].css[attribut] + '">' + code.match(pattern)[match] + '</span>'
+									)
+
+									code = code.replace(
+										new RegExp("\\b" + code.match(pattern)[0] + "\\b", "gm"),
+										matcher
+									)
+								}
+								
+							})
+						}
+					} else {
+						code = code.replace(
+							new RegExp("\\b" + pattern + "\\b", "gm"),
+							'<span style="' + attribut.replace(/\_/gm, "-") + ': ' + syntaxes[group].css[attribut] + '">$&</span>'
+						)
+					}
+					
+				}
+		
+			});
+		}
+
+		codes[i].innerHTML = code
+	}
+}
